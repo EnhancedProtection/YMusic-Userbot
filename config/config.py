d3d5d236dae3c350fce4c5724d08cae9b2c7e363
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-07-22 15:02:53 +0530

    fix: config values

--- config/config.py
+++ config/config.py
@@ -1,15 +1,15 @@
 import os
 
 
-API_ID: int = int(os.getenv("API_ID")) or int()
+API_ID: int = int(os.getenv("API_ID", 0))
 
-API_HASH: str = os.getenv("API_HASH") or str("")
+API_HASH: str = os.getenv("API_HASH", "")
 
-SESSION_STRING: str = os.getenv("SESSION_STRING") or str("")
+SESSION_STRING: str = os.getenv("SESSION_STRING", "")
 
-OWNER_ID: list[int] = [int(os.getenv("OWNER_ID"))] or []
+OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 0))]
 
-LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID")) or int()
+LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", 0))
 
 PREFIX: str = str(".")
 
